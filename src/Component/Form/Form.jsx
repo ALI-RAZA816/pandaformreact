8da775a043db776fb76b90275e886d9ff3ad2b80
ALI RAZA <razadeveloper816@gmail.com>
2025-11-04 03:01:38 -0800

    Update Form.jsx

--- src/Component/Form/Form.jsx
+++ src/Component/Form/Form.jsx
@@ -1,13 +1,14 @@
 
 import style from './Form.module.css'
 
-function Form({FocusHandler ,BlurHandler, HandBlurHandler, HandFocusHandler,submitHandler}) {
+function Form({name, password, FocusHandler ,BlurHandler, HandBlurHandler, HandFocusHandler,submitHandler}) {
     return (
         <form action="" onSubmit = {submitHandler} className={style.form}>
             <label htmlFor="username">Username:</label>
-            <input type="text" onFocus={FocusHandler} onBlur={BlurHandler} placeholder="Username here..." id="username" className={style.userNameField} />
+            <input type="text" onFocus={FocusHandler} onBlur={BlurHandler}
+ref={name} placeholder="Username here..." id="username" className={style.userNameField} />
             <label htmlFor="password">Password:</label>
-            <input type="password" onFocus={HandFocusHandler} onBlur={HandBlurHandler} placeholder="Password here..." id="password" className={style.userPasswordField} />
+            <input type="password" onFocus={HandFocusHandler} ref = {password} onBlur={HandBlurHandler} placeholder="Password here..." id="password" className={style.userPasswordField} />
             <button type="submit">Login</button>
         </form>
     )
